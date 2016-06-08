566b25306ffb41c968b23d005884aff770090e88
John Richardson <richardsonjm@gmail.com>
2016-06-08 14:56:49 -0400

    User and post models

---
+++ facepad/FISFeedPost.h
@@ -0,0 +1,16 @@
+//
+//  FISFeedPost.h
+//  facepad
+//
+//  Created by John Richardson on 6/8/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISFeedPost : NSObject
+
+@property (nonatomic) NSString *user;
+@property (nonatomic) NSString *date;
+
+@end
---
+++ facepad/FISImagePost.h
@@ -0,0 +1,15 @@
+//
+//  FISImagePost.h
+//  facepad
+//
+//  Created by John Richardson on 6/8/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISFeedPost.h"
+
+@interface FISImagePost : FISFeedPost
+
+@property (nonatomic) UIImage *imageContent;
+
+@end
